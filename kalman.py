136eb7f2eebb93a87b5b8065d7485a9506bfe5e7
Michael Bratsch <michael.bratsch83@gmail.com>
2016-07-01 00:10:26 +0200

    updated version

--- kalman.py
+++ kalman.py
@@ -1,16 +1,22 @@
-import numpy as np
 import logging
+from operator import pos
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 log = logging.getLogger(__name__)
 
+
 class Kalman(object):
+
     def __init__(self, x, P):
         self.x = x
         self.P = P
 
-    def print_state_and_covariance(self, name):
-        log.debug("%s x: %s" % (name, self.x))
-        log.debug("P: \n%s" % self.P)
+        self.print_state_and_covariance("State initial")
+
+        self.update_plotter()
 
     def FT(self, dt):
         return np.transpose(self.F(dt))
@@ -21,7 +27,7 @@ class Kalman(object):
     def filter(self, dt, z, R):
 
         log.debug("Filter input dt: %s, z: %s" % (dt, z))
-        #self.print_state_and_covariance("State before")
+        # self.print_state_and_covariance("State before")
 
         # PREDICT
         xhatminus = np.dot(self.F(dt), self.x)
@@ -35,17 +41,58 @@ class Kalman(object):
 
         # state
         KalmanGain = np.dot(np.dot(Pminus, self.HT(dt)), Sinv)
-        log.debug("x: %s, xhat: %s" %(self.x, xhatminus))
-        if dt < 0:
-            self.x = self.x + np.dot(KalmanGain, ytilde)
-        else:
-            self.x = xhatminus + np.dot(KalmanGain, ytilde)
-        self.P = np.dot(np.identity(len(self.x)) - np.dot(KalmanGain,self.H(dt)), Pminus)
+        self.x = xhatminus + np.dot(KalmanGain, ytilde)
+        self.P = np.dot(
+            np.identity(len(self.x)) - np.dot(KalmanGain, self.H(dt)), Pminus)
 
+        self.update_plotter(z)
         self.print_state_and_covariance("State after")
 
+    def print_state_and_covariance(self, name):
+        log.debug("%s x: %s" % (name, self.x))
+        log.debug("P: \n%s" % self.P)
 
-class LowSpeed2dSecondOrder(Kalman):
+
+class Plot2dMixin(object):
+
+    # plot initial state
+    positions = []
+    speeds = []
+    measurements = []
+
+    def update_plotter(self, measurement=None):
+        self.positions.append(self.get_position())
+        self.speeds.append(self.get_speed())
+        if measurement is not None:
+            self.measurements.append(measurement)
+
+    def plot(self):
+        fig = plt.figure()
+        axes = fig.add_subplot(111)
+
+        x, y = zip(*self.positions)
+        axes.plot(x, y, marker='o')
+
+        x, y = zip(*self.measurements)
+        axes.plot(x, y, 'ro')
+
+        for pos, speed in zip(self.positions, self.speeds):
+            if speed:
+                x, y = pos
+                dx, dy = speed
+                axes.plot([x, x + dx], [y, y + dy], color='red')
+
+        plt.show()
+
+
+class LowSpeed2dSecondOrder(Kalman, Plot2dMixin):
+
+    def get_position(self):
+        return self.x[0], self.x[1]
+
+    def get_speed(self):
+        return self.x[2], self.x[3]
+
     def F(self, dt):
         return np.array([[1.0, 0.0, dt, 0.0],
                          [0.0, 1.0, 0.0, dt],
--- runner.py
+++ runner.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
 
-from kalman import LowSpeed2dSecondOrder
-import numpy as np
 import logging
 import math
+
+from kalman import LowSpeed2dSecondOrder
+import numpy as np
+
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 def correlation(a, b):
-    return math.sqrt(a*b)
+    return math.sqrt(a * b)
 
 # INITIAL STATE
 x = np.array([0.0, 0.0, 0.0, 0.0])
 
 sigma_x_P = 1.0
 sigma_y_P = 1.0
-sigma_x_y_P = 0.9 * correlation(sigma_x_P, sigma_y_P)
+sigma_x_y_P = -0.9 * correlation(sigma_x_P, sigma_y_P)
 
 P = np.array([[sigma_x_P, sigma_x_y_P, 0.0, 0.0],
               [sigma_x_y_P, sigma_y_P, 0.0, 0.0],
@@ -26,14 +28,17 @@ P = np.array([[sigma_x_P, sigma_x_y_P, 0.0, 0.0],
 low_speed = LowSpeed2dSecondOrder(x=x, P=P)
 
 
-# MEASUREMENT
-z = np.array([1, 0])
+for i in range(1, 20):
+    # MEASUREMENT
+    z = np.array([i, 0])
 
-sigma_x_R = 1.0
-sigma_y_R = 3.0
-sigma_x_y_R = 0.9 * correlation(sigma_x_R, sigma_y_R)
+    sigma_x_R = 10.0
+    sigma_y_R = 1.2
+    sigma_x_y_R = -0.9 * correlation(sigma_x_R, sigma_y_R)
 
-R = np.array([[sigma_x_R, sigma_x_y_R],
-              [sigma_x_y_R, sigma_x_R]])
+    R = np.array([[sigma_x_R, sigma_x_y_R],
+                  [sigma_x_y_R, sigma_x_R]])
 
-low_speed.filter(dt=-1.0, z=z, R=R)
+    low_speed.filter(dt=1.0, z=z, R=R)
+
+low_speed.plot()
