8394cd9a214c92e9c190ff38ace323a72f007681
Michael Bratsch <michael.bratsch83@gmail.com>
2016-07-02 22:24:34 +0200

    add model with acceleration

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ import numpy as np
 from plotter import Plot2dMixin
 
 
-class Position2d(Kalman, Plot2dMixin):
+class State1Mearsurement1_2d(Kalman, Plot2dMixin):
 
     def get_position(self):
         return self.x[0], self.x[1]
@@ -28,7 +28,7 @@ class Position2d(Kalman, Plot2dMixin):
                          [0.0, 1.0]])
 
 
-class LowSpeed2d(Kalman, Plot2dMixin):
+class State2Measurement1_2d(Kalman, Plot2dMixin):
 
     def get_position(self):
         return self.x[0], self.x[1]
@@ -55,3 +55,36 @@ class LowSpeed2d(Kalman, Plot2dMixin):
                          [0.0, 1.0, 0.0, 0.0],
                          [0.0, 0.0, 1.0, 0.0],
                          [0.0, 0.0, 0.0, 1.0]])
+
+
+class State3Measurement1_2d(Kalman, Plot2dMixin):
+
+    def get_position(self):
+        return self.x[0], self.x[1]
+
+    def get_position_accuracy(self):
+        return np.array([[self.P[0, 0], self.P[0, 1]],
+                         [self.P[1, 0], self.P[1, 1]]])
+
+    def get_speed(self):
+        return self.x[2], self.x[3]
+
+    def F(self, dt):
+        return np.array([[1.0, 0.0, dt, 0.0, 0.5 * dt**2, 0.0],
+                         [0.0, 1.0, 0.0, dt, 0.0, 0.5 * dt**2],
+                         [0.0, 0.0, 1.0, 0.0, dt, 0.0],
+                         [0.0, 0.0, 0.0, 1.0, 0.0, dt],
+                         [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                         [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+
+    def H(self, dt):
+        return np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                         [0.0, 1.0, 0.0, 0.0, 0.0, 0.0]])
+
+    def Q(self, dt):
+        return np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                         [0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                         [0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                         [0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                         [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                         [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
--- runner.py
+++ runner.py
@@ -3,39 +3,51 @@
 import logging
 import math
 
-from filter import LowSpeed2d, Position2d
+from models import (State1Mearsurement1_2d,
+                    State2Measurement1_2d,
+                    State3Measurement1_2d)
 import numpy as np
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARN)
 
 
 def correlation(a, b):
     return math.sqrt(a * b)
 
-# INITIAL STATE
-x1 = np.array([0.0, 0.0, 0.0, 0.0])
+s_xx = 1.0
+s_yy = 1.0
+s_xy = -0.0 * correlation(s_xx, s_yy)
 
-sigma_xx_P = 1.0
-sigma_yy_P = 1.0
-sigma_xy_P = -0.0 * correlation(sigma_xx_P, sigma_yy_P)
 
-P1 = np.array([[sigma_xx_P, sigma_xy_P, 0.0, 0.0],
-               [sigma_xy_P, sigma_yy_P, 0.0, 0.0],
+x1 = np.array([0.0, 0.0])
+P1 = np.array([[s_xx, s_xy],
+               [s_xy, s_yy]])
+
+position2d = State1Mearsurement1_2d(x=x1, P=P1)
+
+
+x2 = np.array([0.0, 0.0, 0.0, 0.0])
+P2 = np.array([[s_xx, s_xy, 0.0, 0.0],
+               [s_xy, s_yy, 0.0, 0.0],
                [0.0, 0.0, 1.0, 0.0],
                [0.0, 0.0, 0.0, 1.0]])
 
-low_speed2d = LowSpeed2d(x=x1, P=P1)
+low_speed2d = State2Measurement1_2d(x=x2, P=P2)
 
 
-x2 = np.array([0.0, 0.0])
-P2 = np.array([[sigma_xx_P, sigma_xy_P],
-               [sigma_xy_P, sigma_yy_P]])
+x3 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+P3 = np.array([[s_xx, s_xy, 0.0, 0.0, 0.0, 0.0],
+               [s_xy, s_yy, 0.0, 0.0, 0.0, 0.0],
+               [0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+               [0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+               [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+               [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
 
-position2d = Position2d(x=x2, P=P2)
+acceleration2d = State3Measurement1_2d(x=x3, P=P3)
 
 
-for filter_2d in [low_speed2d, position2d]:
+for filter_2d in [position2d, low_speed2d, acceleration2d]:
     for i in range(1, 10):
         # MEASUREMENT
         z = np.array([i, 0])
@@ -49,6 +61,7 @@ for filter_2d in [low_speed2d, position2d]:
 
         filter_2d.filter(dt=1.0, z=z, R=R)
 
-low_speed2d.plot(212)
-position2d.plot(211)
-Position2d.show()
+position2d.plot(311)
+low_speed2d.plot(312)
+acceleration2d.plot(313)
+State1Mearsurement1_2d.show()
